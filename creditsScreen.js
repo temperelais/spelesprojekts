ccf3fcffe595a93db56c3a7526c0be41126e8a40
temperelais <149486977+temperelais@users.noreply.github.com>
2024-05-13 21:54:01 +0300

    pievienota poga creditos

--- creditsScreen.js
+++ creditsScreen.js
@@ -1,17 +1,28 @@
 var CCanvas = document.getElementById("creditScreen");
 var CCtx = CCanvas.getContext("2d");
 
-// Set font size and family
+
 CCtx.font = "36px Arial";
-
-// Set text alignment to center
 CCtx.textAlign = "center";
-
-// Set text baseline to middle
 CCtx.textBaseline = "middle";
-
-// Set fill style to white
 CCtx.fillStyle = "white";
 
-// Draw the text
 CCtx.fillText("Spēli veidoja Nikanders, Lukass un Ralfs", CCanvas.width / 2, CCanvas.height / 2);
+var backButtonImg = new Image();
+backButtonImg.src = "atgrBut.png";
+
+backButtonImg.onload = function() {
+    CCtx.drawImage(backButtonImg, CCanvas.width / 4, CCanvas.height / 2 + 100, 200, 100);
+};
+
+CCanvas.addEventListener("click", function(event) {
+    var rect = CCanvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+
+    if (x >= CCanvas.width / 4 && x <= CCanvas.width / 4 + 200 &&
+        y >= CCanvas.height / 2 + 100 && y <= CCanvas.height / 2 + 200) {
+        CCanvas.style.display = "none";
+        startCanvas.style.display = "block";
+    }
+});
