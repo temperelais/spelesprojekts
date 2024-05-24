d1bd8303e6475d9e08e417e6407d9b4387d50a58
temperelais <149486977+temperelais@users.noreply.github.com>
2024-05-24 15:21:45 +0300

    komentāri pievienoti

--- creditsScreen.js
+++ creditsScreen.js
@@ -1,20 +1,20 @@
 var CCanvas = document.getElementById("creditScreen");
 var CCtx = CCanvas.getContext("2d");
 
-
+// definēti burti, kur būs teksts
 CCtx.font = "36px Arial";
 CCtx.textAlign = "center";
 CCtx.textBaseline = "middle";
 CCtx.fillStyle = "white";
-
-CCtx.fillText("Spēli veidoja Nikanders, Lukass un Ralfs", CCanvas.width / 2, CCanvas.height / 2);
+// Credits teksts, kur aprakstīts, kas veidoja spēli
+CCtx.fillText("Spēli veidoja Nikanders, Lukass un Ralfs un Toms", CCanvas.width / 2, CCanvas.height / 2);
 var backButtonImg = new Image();
 backButtonImg.src = "atgrBut.png";
 
 backButtonImg.onload = function() {
     CCtx.drawImage(backButtonImg, CCanvas.width / 4, CCanvas.height / 2 + 100, 200, 100);
 };
-
+// Poga, lai atgrieztos uz sākuma canvasu
 CCanvas.addEventListener("click", function(event) {
     var rect = CCanvas.getBoundingClientRect();
     var x = event.clientX - rect.left;
